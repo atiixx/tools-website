22b0abee7a8e6d8cedb2b6a6db22c6f20fb4da42
Jonas Hauenstein <jonas.hauenstein@outlook.de>
2024-11-12 15:46:21 +0100

    feat: added api card

--- src/app/api-stuff/api-stuff.component.ts
+++ src/app/api-stuff/api-stuff.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { AgeGuessingComponent } from './age-guessing/age-guessing.component';
 import { CatFactsComponent } from './cat-facts/cat-facts.component';
 import { ChuckNorrisJokesComponent } from './chuck-norris-jokes/chuck-norris-jokes.component';
+import { ApiCardComponent } from './api-card/api-card.component';
 
 @Component({
   selector: 'app-api-stuff',
   standalone: true,
-  imports: [AgeGuessingComponent, CatFactsComponent, ChuckNorrisJokesComponent],
+  imports: [
+    AgeGuessingComponent,
+    CatFactsComponent,
+    ChuckNorrisJokesComponent,
+    ApiCardComponent,
+  ],
   templateUrl: './api-stuff.component.html',
   styleUrl: './api-stuff.component.scss',
 })
